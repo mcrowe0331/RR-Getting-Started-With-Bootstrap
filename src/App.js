d580d17d774947e8d1b0f60704e783e705750f38
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-11-01 18:58:46 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <Nav activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Home</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="/about">About Us</Nav.Link>
+    </Nav.Item>
+</Nav>
+
+
     </div>
   );
 }
 
 export default App;
 
-//
+
