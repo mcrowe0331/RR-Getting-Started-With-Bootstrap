d5eb55735581e2402bb0a383c72c2383b7f0921d
Mary Elizabeth <maryelizabeth@Marys-MacBook-Pro.local>
2022-11-01 20:16:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
 
 
 function App() {
@@ -15,6 +16,44 @@ function App() {
       <Nav.Link eventKey="/about">About Us</Nav.Link>
     </Nav.Item>
 </Nav>
+
+
+  <Form>
+
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Form.Group>
+    <Form.Label>Form Dropdown with favorite movies</Form.Label>
+    <Form.Control as="select">
+      <option>Inception</option>
+      <option>Forrest Gump</option>
+      <option>Shawshank Redemption</option>
+      <option>Spiderman: Into the Spiderverse</option>
+      <option>Avengers: Endgame</option>
+    </Form.Control>
+</Form.Group>
+
+
+
+    <Form.Group controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+</Form>
 
 
     </div>
